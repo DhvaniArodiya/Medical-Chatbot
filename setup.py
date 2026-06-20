bf52f17085683cb9dc22046637f34b3162aa331e
DhvaniArodiya <22aidhv003@ldce.ac.in>
2026-06-20 15:23:22 +0530

    web app completed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup 
+
+setup(
+    name = "medical-chatbot",
+    version = "0.1.0",
+    author = "Dhvani Arodiya",
+    author_email = "dhvani.arodiya@.com",
+    packages = find_packages(),
+    install_requires = []
+)
